1df58e717b71c32ef93e0d90bdfcff9a1847463f
0x0de <p_0de-music@outlook.com>
2024-09-19 16:24:49 -0400

    Merging into main...

--- src/flacutils.h
+++ src/flacutils.h
@@ -22,49 +22,6 @@ struct FLAC_frame
 typedef long long int64;
 typedef unsigned long long uint64;
 
-/*
-class bit_reader
-{
-    public:
-        bit_reader(const char* filepath, UINT16 cache_size);
-        ~bit_reader();
-
-        void clear_cache();
-        void read_cache();
-
-        UINT16 get_cache_size() const { return cache_size; }
-
-        const char* get_filepath() const { return filepath; }
-        UINT64 get_file_size() const { return filesize; }
-
-        UINT64 get_bit_index() const { return bit_index; }
-        void set_bit_index(UINT64 index);
-        
-        void increment_bit_index(INT64 value);
-        void jump_to_next_byte();
-
-        bool read_bit();
-        void read_bits(bool* buffer, UINT8 length);
-        INT64 read_as_int(UINT8 bit_length, bool is_signed);
-
-        UINT64 read_unary(bool end_bit);
-        UINT64 read_utf8();
-
-        double elapsed_cache_read_time, elapsed_cache_total_above_1;
-    private:
-        const char* filepath;
-        UINT16 cache_size;
-        UINT32 cache_bit_size;
-
-        FILE* reader;
-        UINT64 bit_index;
-        UINT64 filesize;
-
-        char* cache;
-        inline char read_next_byte();
-};
-*/
-
 class bit_reader
 {
     public:
