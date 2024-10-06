4bae2e4e39dab59983ac43b2038129a0950a2af2
0x0de <p_0de-music@outlook.com>
2024-10-06 14:21:36 -0400

    Working on test_05.

--- tests/05_stressing_vorbis.cpp
+++ tests/05_stressing_vorbis.cpp
@@ -9,23 +9,45 @@
 
 int main()
 {
+    unsigned int src_1_timer = 0, src_2_timer = 0;
+
     crest::init(50);
 
-    crest::audio_source src;
-    crest::register_source(&src);
+    crest::audio_source src_1;
+    crest::register_source(&src_1);
+
+    crest::audio_source src_2;
+    crest::register_source(&src_2);
 
     crest::effect_volume volume(0.25f);
 
-    crest::vorbis_stream stream("./tests/audio/jingle.ogg");
+    crest::vorbis_stream stream_1("./tests/audio/jingle.ogg");
+    crest::vorbis_stream stream_2("./tests/audio/triwave.ogg");
 
-    src.add_transform(&volume);
+    src_1.add_transform(&volume);
+    src_2.add_transform(&volume);
 
     while(true)
     {
-        std::cout << "?" << std::endl;
-        src.add_stream(&stream);
-        std::this_thread::sleep_for(std::chrono::duration<int, std::milli>(500));
+        if(src_1_timer == 100)
+        {
+            src_1.add_stream(&stream_1);
+            src_1_timer = 0;
+        }
+        if(src_2_timer == 33)
+        {
+            src_2.add_stream(&stream_2);
+            src_2_timer = 0;
+        }
+
+        std::this_thread::sleep_for(std::chrono::duration<int, std::milli>(10));
+
+        src_1_timer += 1;
+        src_2_timer += 1;
     }
+
+    crest::remove_source(&src_1);
+    crest::remove_source(&src_2);
 
     return 0;
 }
